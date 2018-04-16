5cbb5cd585624be895b8706e82215c409f8ec24d
Daniel Arrizza <daniel.arrizza@appdynamics.com>
2018-04-16 19:22:23 -0400

    add legal header

--- frontend/src/appKeyFilter.js
+++ frontend/src/appKeyFilter.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2018. AppDynamics LLC and its affiliates.
+ * All Rights Reserved.
+ * This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ * The copyright notice above does not evidence any actual or intended publication of such source code.
+ */
+
 export default (process.env.REACT_APP_APP_KEY
   ? `AND appkey = "${process.env.REACT_APP_APP_KEY}"`
   : '')
